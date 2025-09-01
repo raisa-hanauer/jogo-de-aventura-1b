dc3fb100db413949f0039ca8e6fd09a13f54152a
raisa-hanauer <raisa.deschiavone@escola.pr.gov.br>
2025-09-01 17:04:52 -0300

    adicionamos ações aos botoens

---
+++ script.js
@@ -0,0 +1,11 @@
+const avansa = document.querySelectorAll('.btn-proximos')
+
+avansa.forEach(button => {
+    button.addEventListener('clik', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
